c5945bf52b8b9e95870f160524be9ff2a3dd2870
Bunje Kelly <kellybunje@gmail.com>
2024-01-23 03:01:40 -0800

    seearching a topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -8,4 +8,4 @@ from pymongo import MongoClient
 def schools_by_topic(mongo_collection, topic):
     """function definition"""
     result = mongo_collection.find({"topic": topic})
-    return [doc for doc in result]
+    return [school["name"] for school in result if "name" in school]
