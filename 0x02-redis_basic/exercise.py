e8a950fc428a5adda73aea901d4fa2296a9c1020
Bunje Kelly <kellybunje@gmail.com>
2024-01-26 17:07:21 +0000

    correction

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,9 +11,11 @@ class Cache:
         """initialization"""
         self._redis = redis.Redis()
         self._redis.flushdb()
-
+    
+    @count_calls
+    @call_history
     def store(self, data: str or bytes or int or float) -> str:
         """takes a data argument and returns a string"""
-        random_key = str(uuid.uuid4())
-        self._redis.set(random_key, data)
-        return random_key
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
